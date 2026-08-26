9e72834a083325238a28104b4634272363efbd5a
chroxi <nils.hold@iu-study.org>
2026-08-26 03:53:17 +0200

    feat: accept media directory argument

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("RandomSlide starting...")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: randomslide <directory>")
+		os.Exit(1)
+	}
+	fmt.Println(os.Args[1])
 }
