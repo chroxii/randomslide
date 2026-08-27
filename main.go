296fc7c3fe1a73beebd39216aa23f998c10ebc91
chroxii <321186448+chroxii@users.noreply.github.com>
2026-08-28 01:11:48 +0200

    feat: validate media directory path

--- main.go
+++ main.go
@@ -11,5 +11,21 @@ func main() {
 		fmt.Println("Usage: randomslide <directory>")
 		os.Exit(1)
 	}
-	fmt.Println(os.Args[1])
+
+	pathFromUser := os.Args[1]
+
+	info, err := os.Stat(pathFromUser)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Error: path does not exist")
+		} else {
+			fmt.Println("An error occurred:", err)
+		}
+		os.Exit(1)
+	}
+	if !info.IsDir() {
+		fmt.Println("Error: path is not a directory")
+		os.Exit(1)
+	}
+	fmt.Println("Media directory:", pathFromUser)
 }
